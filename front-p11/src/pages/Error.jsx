b8d61186bd5abd1c303cd82c52700e9c1e3af22b
Emma B <emm2111@hotmail.fr>
2023-10-09 12:19:35 +0200

    Style page d'erreur

--- front-p11/src/pages/Error.jsx
+++ front-p11/src/pages/Error.jsx
@@ -2,7 +2,7 @@ import "../style/main.css"
 
 function Error() {
     return (
-        <div className="page__ctn error">
+        <div className="page__ctn-error">
             <h1 className="error__title">404</h1>
             <p className="error__txt">Oups! La page que vous demandez n'existe pas.</p>
             <a className="error__link" href="/">Retourner sur la page d'accueil
